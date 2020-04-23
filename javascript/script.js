2e11bc75a05fcaf8361f0bf3b7712876229b7d88
Harbimz <63602412+Harbimz@users.noreply.github.com>
2020-04-23 17:05:03 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function menuFunction() {
+  document.getElementById("myMenu").classList.toggle("show");
+}
